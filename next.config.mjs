05a5192514413fda15f52fcec0fddc23e4f8e21e
seth-way <seth.r.way@gmail.com>
2024-02-29 16:57:14 -0500

    troubleshoot biuld issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export' };
+const nextConfig = {
+  output: 'export',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
 
 export default nextConfig;
