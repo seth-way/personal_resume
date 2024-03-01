e9299948a372c7d9cc1fefdebbeaf291c2a98768
seth-way <seth.r.way@gmail.com>
2024-02-29 19:57:19 -0500

    update config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = { output: 'export', images: { unoptimized: true } };
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  console.log('\n\nREPO ????\n\n', repo);
+
+  assetPrefix = `/${repo}/`;
+  console.log('\n\nASSET PREFIX ????\n\n', assetPrefix);
+  basePath = `/${repo}`;
+  console.log('\n\nBASEPATH ????\n\n', basePath);
+}
+
+const nextConfig = isGithubActions
+  ? { basePath: basePath, output: 'export', images: { unoptimized: true } }
+  : { output: 'export', images: { unoptimized: true } };
 
 export default nextConfig;
