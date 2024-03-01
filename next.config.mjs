76dd29c2996bedd5d586b5378a456d341694dc5f
seth-way <seth.r.way@gmail.com>
2024-02-29 19:33:03 -0500

    render images statically

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: 'wayglobal-338596864.imgix.net',
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
