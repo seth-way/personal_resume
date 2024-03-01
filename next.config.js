474ba625b9679b77e0eeaa55d3ae758a5c5b2c24
seth-way <seth.r.way@gmail.com>
2024-02-29 21:25:30 -0500

    vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,27 +1,7 @@
-const isGithub =
-  process.env.GITHUB_ACTIONS || process.env.GITHUB_PAGES || false;
-
-console.log('\nIS GITHUB??\n', isGithub);
-if (isGithub) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
-  console.log('\n\nREPO ????\n\n', repo);
-
-  assetPrefix = `/${repo}/`;
-  console.log('\n\nASSET PREFIX ????\n\n', assetPrefix);
-  basePath = `/${repo}`;
-  console.log('\n\nBASEPATH ????\n\n', basePath);
-}
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = isGithub
-  ? {
-      basePath: '',
-      output: 'export',
-      images: { unoptimized: true },
-    }
-  : { output: 'export', images: { unoptimized: true } };
+const nextConfig = {
+};
 
 module.exports = nextConfig;
